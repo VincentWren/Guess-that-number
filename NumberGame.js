ed62ba5d734eb453b4b394a34bfef8a32bce8570
Vincent Wren <vincentwren@BCCSs-MacBook-Pro.local>
2023-02-23 10:40:00 -0600

    Created the script file, Created project variables

---
+++ NumberGame.js
@@ -0,0 +1,22 @@
+// GUESS THAT NUMBER
+// Message to be used throughout the project/file
+const enterNumText = `Please enter a number greater thn zero`;
+
+// For starting, restarting and ending the game
+let restartGame = true;
+
+// For storing the range that the user will guess
+let rangeNum;
+
+// For storing the number that the user will attempt to guess
+let randomNum;
+
+// For storing the number of attempts the user has left
+let attempts;
+
+// For storing a user's guess
+let guess;
+
+// For storing the user's response to play again or NOT play again
+let playAgain;
+
